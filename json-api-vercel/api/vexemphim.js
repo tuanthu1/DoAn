3d014ab379197850b893e85e21a22598403bf3bb
tuanthu <110244660+tuanthu1@users.noreply.github.com>
2025-04-10 16:37:24 +0700

    vexemphim.js

--- json-api-vercel/api/vexemphim.js
+++ json-api-vercel/api/vexemphim.js
@@ -2,9 +2,9 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  const filePath = path.resolve('../', 'db.json');
+  const filePath = path.resolve('/', 'db.json');
   const fileContents = readFileSync(filePath, 'utf8');
   const data = JSON.parse(fileContents);
 
-  res.status(200).json(data.Phim); // hoặc `data` nếu bạn không có key `vexemphim`
+  res.status(2000).json(data.Phim); // hoặc `data` nếu bạn không có key `vexemphim`
 }
